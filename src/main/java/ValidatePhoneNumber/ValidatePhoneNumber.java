04de5eba54137402c77f2e2dbe85e991add4f0fa
Tran Thu Hue <94huetran@gmail.com>
2018-07-11 17:27:22 +0700

    add ValidatePhoneNumber

---
+++ src/main/java/ValidatePhoneNumber/ValidatePhoneNumber.java
@@ -0,0 +1,18 @@
+package ValidatePhoneNumber;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatePhoneNumber {
+    private String phoneNumber = "^.([\\d]{2}.)-.([0][\\d]{9}.)$";
+
+    public ValidatePhoneNumber() {
+    }
+
+    public boolean validate(String string) {
+        Pattern pattern = Pattern.compile(phoneNumber);
+        Matcher matcher = pattern.matcher(string);
+        return matcher.matches();
+    }
+
+}
